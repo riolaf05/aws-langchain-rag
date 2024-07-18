5fb2bfd99d4180b0e8b403782871f44bb8987158
Rosario Laface <lafacerosario@gmail.com>
2024-07-18 16:23:32 +0200

    adding document loader

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from logging import Logger
 
 from api.routers import (
+    embed_documents_router,
     embed_image_router,
     upload_router,
     process_router
@@ -16,6 +17,7 @@ app = FastAPI(
 app.include_router(upload_router.router)
 app.include_router(process_router.router)
 app.include_router(embed_image_router.router)
+app.include_router(embed_documents_router.router)
 
 if __name__ == "__main__":
     run("main:app", host="0.0.0.0", port=3000, reload=True)
